e219f9016eb1df4c3ce263e3b9a0753f25fe70d9
kimmoperala <kimmoperala@gmail.com>
2020-09-07 15:12:57 +0300

    Updates

---
+++ precache-manifest.125d5267ba9139ecd6d89ee9e6d08d2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c74633290fac163ae4b43d6f3444d8a",
+    "url": "/PublicAPIs/index.html"
+  },
+  {
+    "revision": "f708aaae7f75f4d45fc9",
+    "url": "/PublicAPIs/static/js/2.d275c0f4.chunk.js"
+  },
+  {
+    "revision": "b818062cb6a02d6d46e1d6036acdfc48",
+    "url": "/PublicAPIs/static/js/2.d275c0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57659fc3c8d6d1124578",
+    "url": "/PublicAPIs/static/js/main.5aac09f2.chunk.js"
+  },
+  {
+    "revision": "3aa29af6c09cd1d7ff8b",
+    "url": "/PublicAPIs/static/js/runtime-main.9f956c24.js"
+  }
+]);
